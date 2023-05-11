164acf188610563d1f99d70f6119432eddb6a322
IchsanAlfian <eksanalfian@gmail.com>
2023-05-11 10:46:04 +0700

    checkpoint add galeri action

--- app/src/main/java/com/ichsanalfian/mystoryapp/ui/addStory/AddStoryActivity.kt
+++ app/src/main/java/com/ichsanalfian/mystoryapp/ui/addStory/AddStoryActivity.kt
@@ -2,8 +2,11 @@ package com.ichsanalfian.mystoryapp.ui.addStory
 
 import android.Manifest
 import android.content.Intent
+import android.content.Intent.ACTION_GET_CONTENT
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -11,13 +14,18 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ichsanalfian.mystoryapp.R
 import com.ichsanalfian.mystoryapp.databinding.ActivityAddStoryBinding
 import com.ichsanalfian.mystoryapp.databinding.ActivityStoryBinding
+import com.ichsanalfian.mystoryapp.utils.createCustomTempFile
+import com.ichsanalfian.mystoryapp.utils.uriToFile
+import java.io.File
 
 class AddStoryActivity : AppCompatActivity() {
+    private lateinit var currentPhotoPath: String
     private lateinit var binding: ActivityAddStoryBinding
     companion object {
         private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
@@ -29,6 +37,7 @@ class AddStoryActivity : AppCompatActivity() {
         requestPermission()
 
         binding.cameraButton.setOnClickListener { startTakePhoto() }
+        binding.galleryButton.setOnClickListener { startGallery() }
 
     }
 
@@ -70,13 +79,45 @@ class AddStoryActivity : AppCompatActivity() {
         ActivityResultContracts.StartActivityForResult()
     ) {
         if (it.resultCode == RESULT_OK) {
-            val imageBitmap = it.data?.extras?.get("data") as Bitmap
-            binding.previewImageView.setImageBitmap(imageBitmap)
+            val myFile = File(currentPhotoPath)
+
+            myFile.let { file ->
+//          Silakan gunakan kode ini jika mengalami perubahan rotasi
+//          rotateFile(file)
+                binding.previewImageView.setImageBitmap(BitmapFactory.decodeFile(file.path))
+            }
         }
     }
     private fun startTakePhoto() {
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        launcherIntentCamera.launch(intent)
+        intent.resolveActivity(packageManager)
+        createCustomTempFile(application).also {
+            val photoURI: Uri = FileProvider.getUriForFile(
+                this@AddStoryActivity,
+                "com.ichsanalfian.mystoryapp",
+                it
+            )
+            currentPhotoPath = it.absolutePath
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+            launcherIntentCamera.launch(intent)
+        }
     }
-
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val selectedImg = result.data?.data as Uri
+            selectedImg.let { uri ->
+                val myFile = uriToFile(uri, this@AddStoryActivity)
+                binding.previewImageView.setImageURI(uri)
+            }
+        }
+    }
+    private fun startGallery() {
+        val intent = Intent()
+        intent.action = ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Silahkan Pilih Gambar")
+        launcherIntentGallery.launch(chooser)
+    }
 }
